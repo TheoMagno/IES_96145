826049d0d6ce65f78df5e799d812c7323e76231f
TheoMagno <theomagno2016@hotmail.com>
2021-11-02 15:10:10 +0000

    Exercísio 2 quase pronto e primeiro commit exercísio 3

---
+++ lab2/lab2_3/lab2_3_b/src/main/java/com/example/demo/Lab23BApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab23BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab23BApplication.class, args);
+	}
+
+}
